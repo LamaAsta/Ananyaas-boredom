1fc30b2f4f84f34f3c4f1128cdf0d8e4077ff476
LamaAsta <amalformal@gmail.com>
2025-02-08 11:40:36 +0530

    the puzzle 2x2 is now run 20 times

--- choice.py
+++ choice.py
@@ -77,6 +77,73 @@ class washWindow:
         return
     
 
+class boredWindow:
+    def __init__(self):
+        self.end = False
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.font = pygame.font.Font(None, 100)
+
+    def new(self):
+        self.buttons_list = []
+        self.buttons_list.append(Button(200+20, 300, 150, 100, "1", WHITE, BLACK))
+        self.buttons_list.append(Button(200+190, 300, 150, 100, "2", WHITE, BLACK))
+        self.buttons_list.append(Button(200+360, 300, 150, 100, "3", WHITE, BLACK))
+        self.buttons_list.append(Button(200+530, 300, 150, 100, "4", WHITE, BLACK))
+        self.buttons_list.append(Button(200+700, 300, 150, 100, "5", WHITE, BLACK))
+        self.screen.fill(BGCOLOUR)
+        for i in self.buttons_list:
+            i.draw(self.screen)
+        
+        text = "How bored are you on a scale of"
+        x, y = 100, 100
+        text = self.font.render(text, True, WHITE)
+        text1 = "1 to 5"
+        x1, y1 = 550, 170
+        text1 = self.font.render(text1, True, WHITE)
+        self.screen.blit(text, (x, y))
+        self.screen.blit(text1, (x1, y1))
+        pygame.display.flip()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                for button in self.buttons_list:
+                    if button.click(mouse_x, mouse_y):
+                        if button.text == "1":
+                            self.value = 1
+                            self.end = True
+                        if button.text == "2":
+                            self.value = 2
+                            self.end = True
+                        if button.text == "3":
+                            self.value = 3
+                            self.end = True
+                        if button.text == "4":
+                            self.value = 4
+                            self.end = True
+                        if button.text == "5":
+                            self.value = 5
+                            self.end = True
+    
+    def run(self):
+        while not self.end:
+            self.events()
+        return self.value
+
+
+# while True:
+#     a.new()
+#     c = a.run()
+#     print(c)           
+#     pygame.quit()
+#     break
+
 # while True:
 #     choice.new()
 #     c = choice.run()
